3be23025de572e662647c12b3a4b7498b9e1756d
Pavel Koryakin <unclekoin@gmail.com>
2021-06-06 21:24:22 +0300

    feat: finish project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 import "./App.css";
 
 const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          name: userName,
+          age: userAge,
+          id: 'uid-' + Math.random().toString().replace(/\./, '')
+        }
+      ];
+    })
+  }
+
   return (
     <div className="app">
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
     </div>
   )
 }

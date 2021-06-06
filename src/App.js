66405e9af9d832e25c8ebb4a544d80689a419423
Pavel Koryakin <unclekoin@gmail.com>
2021-06-06 13:00:48 +0300

    feat: add AddUser component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import AddUser from "./components/Users/AddUser";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="app"></div>
+    <div className="app">
+      <AddUser />
+    </div>
   )
 }
 

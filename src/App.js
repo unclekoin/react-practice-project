c69c82adc5af6ec1186402dc09589bfbb88bca5f
Pavel Koryakin <unclekoin@gmail.com>
2021-06-06 17:01:22 +0300

    feat: add UsersList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
       <AddUser />
+      <UsersList users={[]} />
     </div>
   )
 }
